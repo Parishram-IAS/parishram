e8250b27a96fc0d96c3dec6eb20a563668fdeb0b
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2020-06-23 20:41:33 +0530

    Inital Next js Setup

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import 'semantic-ui-css/semantic.min.css'
+import '../public/styles/style.css'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
