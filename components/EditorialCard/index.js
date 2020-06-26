f7ae388cf9ff6afafbac52bade0f6031dd9db23c
Aravinth-0812 <aravinth.periasamy@chimpfund.com>
2020-06-26 20:15:31 +0530

    firebase integration

--- components/EditorialCard/index.js
+++ components/EditorialCard/index.js
@@ -1,243 +1,78 @@
 import React, { Fragment, useEffect } from 'react';
-import { Card, Header, Image, Grid, Icon, Segment, Container } from 'semantic-ui-react';
+import Link from 'next/link';
+import { Card, Header, Image, Grid, Placeholder } from 'semantic-ui-react';
 
 import styles from './EditorialCard.module.css';
-const EditorialCard = ({ type }) => {
+const EditorialCard = ({ articleList, loader, type }) => {
     const noDataImg = '/assests/svg/book.svg';
-    useEffect(() => {
-        console.log(type);
-    }, []);
 
+    const renderArticleList = () => {
+        return articleList.map((article) => {
+            return (
+                <Card fluid className={styles.cardWraperrBorder} key={article.id}>
+                    <Card.Content>
+                        <Grid>
+                            <Grid.Column
+                                mobile={16}
+                                tablet={2}
+                                computer={2}
+                            >
+                                <Image
+                                    floated="left"
+                                    src={article.featured_image || noDataImg}
+                                    style={{ height: '100px' }}
+                                />
+                            </Grid.Column>
+                            <Grid.Column
+                                mobile={16}
+                                tablet={14}
+                                computer={14}
+                            >
+                                <Card.Header>
+                                    <Header as="h3">
+                                        <Header.Content className={styles.editorialCardTitle}>
+                                            {article.title}
+                                        </Header.Content>
+                                        <Header clearing>
+                                            <Header as='h5' floated='left' className={styles.editoriolTimeText}>
+                                                Time
+                                       </Header>
+                                            <Header as='h5' floated='right' className={styles.readMoreText}>
+                                                <Link href={`/editorial/${type}/${article.id}`}>
+                                                    <a>
+                                                        Read More...
+                                                    </a>
+                                                </Link>
+                                            </Header>
+                                        </Header>
+                                    </Header>
+                                </Card.Header>
+                            </Grid.Column>
+                        </Grid>
+                    </Card.Content>
+                </Card>
+            )
+        })
+    }
     return (
         <Fragment>
-            <Card fluid className={styles.cardWraperrBorder}>
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={2}
-                            computer={2}
-                        >
-                            <Image
-                                floated="left"
-                                src={noDataImg}
-                                style={{ height: '100px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={14}
-                            computer={14}
-                        >
-                            <Card.Header>
-                                <Header as="h3">
-                                    <Header.Content className={styles.editorialCardTitle}>
-                                         An Opportunity of Building an Economy Post COVID-19 Crisis
-                                    </Header.Content>
-                                    <Header clearing>
-                                        <Header as='h5' floated='left' className={styles.editoriolTimeText}>
-                                           Time
-                                        </Header>
-                                        <Header as='h5' floated='right' className={styles.readMoreText}>
-                                        Read More
-                                        </Header>
-                                    </Header>
-                                </Header>
-                            </Card.Header>
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
-            <Card fluid className={styles.cardWraperrBorder}>
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={2}
-                            computer={2}
-                        >
-                            <Image
-                                floated="left"
-                                src={noDataImg}
-                                style={{ height: '100px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={14}
-                            computer={14}
-                        >
-                            <Card.Header>
-                                <Header as="h3">
-                                    <Header.Content className={styles.editorialCardTitle}>
-                                         An Opportunity of Building an Economy Post COVID-19 Crisis
-                                    </Header.Content>
-                                    <Header clearing>
-                                        <Header as='h5' floated='left' className={styles.editoriolTimeText}>
-                                           Time
-                                        </Header>
-                                        <Header as='h5' floated='right' className={styles.readMoreText}>
-                                        Read More
-                                        </Header>
-                                    </Header>
-                                </Header>
-                            </Card.Header>
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
-            <Card fluid>
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={2}
-                            computer={2}
-                        >
-                            <Image
-                                floated="left"
-                                src={noDataImg}
-                                style={{ height: '100px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={14}
-                            computer={14}
-                        >
-                            <Card.Header>
-                                <Header as="h3">
-                                    <Header.Content className={styles.editorialCardTitle}>
-                                         An Opportunity of Building an Economy Post COVID-19 Crisis
-                                    </Header.Content>
-                                    <Header clearing>
-                                        <Header as='h5' floated='left' className={styles.editoriolTimeText}>
-                                           Time
-                                        </Header>
-                                        <Header as='h5' floated='right' className={styles.readMoreText}>
-                                        Read More
-                                        </Header>
-                                    </Header>
-                                </Header>
-                            </Card.Header>
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
-            <Card fluid>
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={2}
-                            computer={2}
-                        >
-                            <Image
-                                floated="left"
-                                src={noDataImg}
-                                style={{ height: '100px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={14}
-                            computer={14}
-                        >
-                            <Card.Header>
-                                <Header as="h3">
-                                    <Header.Content className={styles.editorialCardTitle}>
-                                         An Opportunity of Building an Economy Post COVID-19 Crisis
-                                    </Header.Content>
-                                    <Header clearing>
-                                        <Header as='h5' floated='left' className={styles.editoriolTimeText}>
-                                           Time
-                                        </Header>
-                                        <Header as='h5' floated='right' className={styles.readMoreText}>
-                                        Read More
-                                        </Header>
-                                    </Header>
-                                </Header>
-                            </Card.Header>
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
-            <Card fluid>
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={2}
-                            computer={2}
-                        >
-                            <Image
-                                floated="left"
-                                src={noDataImg}
-                                style={{ height: '100px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={14}
-                            computer={14}
-                        >
-                            <Card.Header>
-                                <Header as="h3">
-                                    <Header.Content className={styles.editorialCardTitle}>
-                                         An Opportunity of Building an Economy Post COVID-19 Crisis
-                                    </Header.Content>
-                                    <Header clearing>
-                                        <Header as='h5' floated='left' className={styles.editoriolTimeText}>
-                                           Time
-                                        </Header>
-                                        <Header as='h5' floated='right' className={styles.readMoreText}>
-                                        Read More
-                                        </Header>
-                                    </Header>
-                                </Header>
-                            </Card.Header>
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
-            <Card fluid>
-                <Card.Content>
-                    <Grid>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={2}
-                            computer={2}
-                        >
-                            <Image
-                                floated="left"
-                                src={noDataImg}
-                                style={{ height: '100px' }}
-                            />
-                        </Grid.Column>
-                        <Grid.Column
-                            mobile={16}
-                            tablet={14}
-                            computer={14}
-                        >
-                            <Card.Header>
-                                <Header as="h3">
-                                    <Header.Content className={styles.editorialCardTitle}>
-                                         An Opportunity of Building an Economy Post COVID-19 Crisis
-                                    </Header.Content>
-                                    <Header clearing>
-                                        <Header as='h5' floated='left' className={styles.editoriolTimeText}>
-                                           Time
-                                        </Header>
-                                        <Header as='h5' floated='right' className={styles.readMoreText}>
-                                        Read More
-                                        </Header>
-                                    </Header>
-                                </Header>
-                            </Card.Header>
-                        </Grid.Column>
-                    </Grid>
-                </Card.Content>
-            </Card>
+            {
+                loader ? (
+                    <Placeholder fluid>
+                        <Placeholder.Header image>
+                            <Placeholder.Line />
+                            <Placeholder.Line />
+                        </Placeholder.Header>
+
+                    </Placeholder>
+                )
+                    : (
+                        (articleList && articleList.length > 0) ? renderArticleList()
+                            : 'No Data to display'
+                    )
+            }
+
+
         </Fragment>
     )
 };
--- services/firebase/article.js
+++ services/firebase/article.js
@@ -1,14 +1,27 @@
 import { firestore } from "./firebase.config";
 
+/**
+ * List all the articles of a particular editorial from database.
+ * @param {string} editorialId name of the editorial.
+ * @param {number} num2 The second number.
+ * @returns {array} article returns all the article of particular editorial.
+ */
 export const getArticleList = async (editorialId) => {
   const querySnapshot = await firestore.collection(`editorial/${editorialId}/article`).get();
-  let article = [];
-//   console.log("val", querySnapshot.val())
+  const articleList = [];
   querySnapshot.forEach((doc) => {
-
-    console.log(`${doc.id} => ${doc.data()}`);
-    console.table(doc.data())
-    article.push(doc.data());
+    articleList.push({ ...doc.data(), id: doc.id });
   });
-  return article;
+  return articleList;
+};
+
+
+export const getIndividualArticle = async (editorialId, slug) => {
+  const doc = await firestore.doc(`editorial/${editorialId}/article/${slug}`).get();
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      return doc.data()
+    } else {
+      return null
+    }
 };
