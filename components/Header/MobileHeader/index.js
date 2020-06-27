a5ce04aa39c808a51a5517b2992e760a11440678
Vivek205 <ichbinvivek@gmail.com>
2020-06-27 12:50:10 +0530

    homepage on logo click

--- components/Header/MobileHeader/index.js
+++ components/Header/MobileHeader/index.js
@@ -1,33 +1,15 @@
-import React from 'react';
-import { Sidebar, Menu } from 'semantic-ui-react';
-
+import React from "react";
+import { Sidebar, Menu } from "semantic-ui-react";
 
 const MobileHeader = ({ visible }) => {
-
-    return (
-        <Sidebar
-            as={Menu}
-            animation='push'
-            direction='left'
-            inverted
-            vertical
-            visible={visible}
-            width='thin'
-        >
-            <Menu.Item as='a'>
-                IAS EXAM
-                    </Menu.Item>
-            <Menu.Item as='a'>
-                CURRENT AFFAIRS
-                    </Menu.Item>
-            <Menu.Item as='a'>
-                EDITORIALS
-                    </Menu.Item>
-            <Menu.Item as='a'>
-                MAINS ANSWER WRITING
-                    </Menu.Item>
-        </Sidebar>
-    )
+  return (
+    <Sidebar as={Menu} animation="push" direction="left" inverted vertical visible={visible} width="thin">
+      <Menu.Item as="a">IAS EXAM</Menu.Item>
+      <Menu.Item as="a">CURRENT AFFAIRS</Menu.Item>
+      <Menu.Item as="a">EDITORIALS</Menu.Item>
+      <Menu.Item as="a">MAINS ANSWER WRITING</Menu.Item>
+    </Sidebar>
+  );
 };
 
 export default MobileHeader;
